f3fadd6a3a5edc2f5313f29c4ebe2905f7f4de74
Taylor Takanishi <taylortakanishi@gmail.com>
2021-06-05 16:05:46 -0700

    13.6.1

--- Earthquakes_past7days/static/js/logicStep1.js
+++ Earthquakes_past7days/static/js/logicStep1.js
@@ -21,20 +21,16 @@ let baseMaps = {
 
 // Create the map object with a center and zoom level
 let map = L.map('mapid', {
-    center: [43.7, -79.3],
-    zoom: 11,
-    layers: [satelliteStreets]
+    center: [39.5, -98.5],
+    zoom: 3,
+    layers: [streets]
 });
 
 // Pass our map layers into out layer control and add it to the map
 L.control.layers(baseMaps).addTo(map);
 
-// Accessing the Toronto neighborhoods GeoJSON URL.
-let torontoHoods = "https://raw.githubusercontent.com/taytaka/Mapping_Earthquakes/main/torontoNeighborhoods.json";
-
-// Grabbing our GeoJSON data.
-d3.json(torontoHoods).then(function(data) {
-    console.log(data);
+// Retreive the earthquake GeoJSON data
+d3.json("https://earthquake.usgs.gov/earthquakes/feed/v1.0/summary/all_week.geojson").then(function(data) {
   // Creating a GeoJSON layer with the retrieved data.
   L.geoJson(data).addTo(map);
 });
